14b3d24b00e3f4722768e674b31ebf77987cec46
H1ken <rokvin@gmail.com>
2019-06-11 22:50:40 +0300

    Finished pawn's textures and started working with grid

--- ToddPawnBase.gd
+++ ToddPawnBase.gd
@@ -6,18 +6,18 @@ extends Sprite
 var black_band = preload("art/black_band.png")
 var white_band = preload("art/white_band.png")
 
+enum BAND_COLOR { NONE, WHITE_BAND, BLACK_BAND }
+export (BAND_COLOR) var wearing = BAND_COLOR.NONE
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	if wearing == BAND_COLOR.WHITE_BAND:
+		$Band.texture = white_band
+	elif wearing == BAND_COLOR.BLACK_BAND:
+		$Band.texture = black_band
+		$Band.flip_h = true
+		flip_h = true
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-# My try to learn init
-func _init(color):
-	var band = get_node("Band")
-	if color == "W":
-		band.texture(white_band)
-	elif color == "B":
-		band.texture(black_band)
