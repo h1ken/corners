835f5be2cff6d266372b90aee7a1ad275daded83
Cristopher Stripeback <cstripeb@at-j4.21-school.ru>
2019-06-12 20:00:18 +0300

    Added grid manipulations + comment modifications

--- Board.gd
+++ Board.gd
@@ -1,5 +1,8 @@
 extends PanelContainer
 
+# Enum for grid
+enum { EMPTY, BLACK, WHITE }
+
 # Consts
 
 # For changing board size
@@ -19,6 +22,7 @@ onready var Pawn = preload("res://ToddPawnBase.tscn")
 
 # Board
 var board_size = Vector2(BOARD_WIDTH, BOARD_HEIGT)
+var board_grid = []
 
 # Needed for manipulation with graphical representation of board
 onready var tile_size = $BoardTiles.get_cell_size()
@@ -30,6 +34,7 @@ onready var tile_center = tile_size / 2
 
 # Override block
 func _ready():
+	init_grid()
 	init_pawns()
 
 func _process(delta):
@@ -41,26 +46,41 @@ func _gui_input(event):
 
 # Block of new functions
 
-# Need to do research about what should I do with "x / PAWNS_IN_ROW", cause I don't think it is good idea to ignore warning
+func init_grid():
+	for x in range(board_size.x):
+		board_grid.append([])
+		for y in range(board_size.y):
+			board_grid[x].append(EMPTY)
+
+# Place pawns on board and fill grid
+#  Need to do research about what should I do with "x / PAWNS_IN_ROW", cause I don't think it is good idea to ignore warning
 func init_pawns():
 	for x in range(PAWNS_QUANTITY):
-			var tmp_todd = Pawn.instance()
-			var tmp_coord = Vector2(x % PAWNS_IN_ROW, x / PAWNS_IN_ROW)
+		# Starting from top-left cell, place black pawn on board
+		var tmp_todd = Pawn.instance()
+		var tmp_coord = Vector2(x % PAWNS_IN_ROW, x / PAWNS_IN_ROW)
 
-			tmp_todd.wearing = tmp_todd.BAND_COLOR.BLACK_BAND
-			tmp_todd.position = $BoardTiles.map_to_world(tmp_coord) + tile_center
-			self.add_child(tmp_todd)
-			
-			tmp_coord += Vector2(1, 1)
-			tmp_todd = Pawn.instance()
-			tmp_todd.wearing = tmp_todd.BAND_COLOR.WHITE_BAND
-			tmp_todd.position = $BoardTiles.map_to_world(board_size - tmp_coord) + tile_center
-			self.add_child(tmp_todd)
+		tmp_todd.wearing = tmp_todd.BAND_COLOR.BLACK_BAND
+		tmp_todd.position = $BoardTiles.map_to_world(tmp_coord) + tile_center
+		board_grid[tmp_coord.x][tmp_coord.y] = BLACK
+		self.add_child(tmp_todd)
+
+		# Starting from bottom-right cell, place white pawn on board
+		tmp_coord = board_size - Vector2(1, 1) - tmp_coord
+		tmp_todd = Pawn.instance()
+
+		tmp_todd.wearing = tmp_todd.BAND_COLOR.WHITE_BAND
+		tmp_todd.position = $BoardTiles.map_to_world(tmp_coord) + tile_center
+		board_grid[tmp_coord.x][tmp_coord.y] = WHITE
+		self.add_child(tmp_todd)
 
 func mouse_handler(event):
 	if event.button_index == BUTTON_LEFT and event.pressed:
 		var pos = $BoardTiles.world_to_map(event.position)
 		
-		# If no chosen pawn
-		
+		# If no chosen pawn, check is there a pawn under cursor
+		# If pawn is chosen and under cursor is another one, return to prev state and choose new pawn
+		# If pawn was moved and chosen again, end turn
+		#   If it wasn't moved, unchoose
+		# If under cursor is possible position, move pawn 
 # Functions END
--- ToddPawnBase.gd
+++ ToddPawnBase.gd
@@ -3,6 +3,16 @@ extends Sprite
 # Enum for adding a band to Todd
 enum BAND_COLOR { NONE, WHITE_BAND, BLACK_BAND }
 
+# Consts
+
+# Pawn movement
+const MOVE_UP = Vector2(0, -1)
+const MOVE_DOWN = Vector2(0, 1)
+const MOVE_LEFT = Vector2(-1, 0)
+const MOVE_RIGHT = Vector2(1, 0)
+
+# Consts END
+
 # Variables
 
 # New option to choose band in editor
@@ -15,6 +25,7 @@ var white_band = preload("art/white_band.png")
 # Some flags to work with
 var is_chosen = false
 var was_moved = false
+var pos = Vector2(-1, -1)
 
 # Variables END
 
@@ -32,5 +43,9 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+# Pawn functions
+func pawn_going_to(direction):
+	pos += direction
 
 # Functions END
